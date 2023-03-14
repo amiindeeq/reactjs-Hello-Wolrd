6048b09c84ef341a8610d46e1626bdbd0e871e17
amiindeeq <amiindeeqxussen@gmail.com>
2023-03-14 19:40:48 +0300

    add .

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return <div>
+        <h2>Hello World</h2>
+        <h3>How Do U doing</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam maiores fuga fugiat quibusdam laudantium delectus assumenda voluptas ad quas consequuntur doloremque debitis ullam dignissimos eaque quidem, excepturi ab obcaecati facilis. Fuga illo architecto totam exercitationem. Deleniti amet qui exercitationem, accusantium labore explicabo iste aliquid totam neque nisi saepe consequuntur possimus quaerat quia quod, ullam eaque laborum error architecto rem molestiae magnam! Architecto qui non dolorem at cum, odio inventore impedit voluptatem explicabo consequatur nobis, deserunt, ex voluptate. Voluptatem ex harum exercitationem atque soluta nobis nostrum ad dolorem voluptates cupiditate vitae asperiores adipisci magni aliquid tempore praesentium fugit corrupti, similique ducimus quae? Quos, facere temporibus quia natus ratione iste illo beatae, tenetur adipisci corporis, non fugiat recusandae! Nesciunt accusantium laborum odio laudantium minus ad cupiditate consequatur doloribus rem id voluptates perspiciatis eius perferendis cum distinctio, porro autem rerum? Quaerat nulla ab est suscipit eius magni possimus ullam, deserunt consectetur iusto ut vero nemo aperiam officia minima eveniet velit delectus laboriosam voluptate error, eos ipsa iste eaque! Adipisci eum voluptates, quos commodi, eaque earum, architecto voluptatem sunt fugiat repellat deserunt iste consectetur. Excepturi aspernatur totam ipsam officiis dolores consectetur animi a cumque corporis, nesciunt explicabo perspiciatis ut aut voluptatem possimus id assumenda.</p>
+    </div>
+
+}
+
+export default App;
